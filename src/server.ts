bcb817d0fe31b082b02a6dcaf1d1d2a12005fd1e
Komalpreet kaur <kkaur209@academic.rrc.ca>
2025-11-10 15:07:07 -0600

    Done some changes

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
-import app from './app';
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+import app from "./app";
+import { Server } from "http";
+
+const PORT: string | number = process.env.PORT || 3000;
+
+const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export { server };
